39b2d461ce685d4fab4e8669beae8bb52c0c2af4
ExoCinki <aolacot@gmail.com>
2026-04-06 17:45:57 +0200

    feat(icon): Added new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,15 @@ export const metadata: Metadata = {
   title: "NW Guild Manager",
   description:
     "Manage ur roster, and payouts for New World Guilds. Made by a guild, for guilds.",
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+  },
 };
 
 export default function RootLayout({
